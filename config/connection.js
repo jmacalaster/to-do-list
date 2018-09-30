808a5ad5d4ccc836cc540009ba87441adbef735d
JMac <julia@defmethod.com>
2018-09-30 17:39:33 -0400

    Heroku Connect

--- config/connection.js
+++ config/connection.js
@@ -4,13 +4,19 @@ var mysql = require("mysql");
 var connection;
 
 if (process.env.JAWSDB_URL) {
-  connection = mysql.createConnection(process.env.JAWSDB_URL);  
+  connection = mysql.createConnection({
+    user: process.env.JAWSDB_USER,
+    password: process.env.JAWSDB_PASS,
+    database: process.env.JAWSDB_BASE,
+    host: process.env.JAWSDB_HOST,
+    port: process.env.JAWSDB_PORT
+  });  
 } else {
   connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "smile",
+    password: process.env.dbPassword,
     database: "todolist_db"
   });
 };
